7223a6b7f981101c0b03d74ab7b776e87db68088
Iuliia Karnausova <iuliia.karnausova@enkod.io>
2025-09-28 18:47:45 +0300

    refactor

--- src/test/java/tests/TestData.java
+++
@@ -1,8 +0,0 @@
-package tests;
-
-import com.github.javafaker.Faker;
-import static utils.RandomUtils.*;
-
-public class TestData {
-    static Faker faker = new Faker();
-}
--- src/test/java/utils/RandomUtils.java
+++
@@ -1,7 +0,0 @@
-package utils;
-
-import com.github.javafaker.Faker;
-
-public class RandomUtils {
-    static Faker faker = new Faker();
-}
